6ff28ca5e6161f4a71d826fc5838052c587384cd
eng-ivan <eng.ivancarlos85@gmail.com>
2023-07-27 12:55:04 -0300

    add client anotation

--- src/main/java/core/ms/client/infra/domain/Client.java
+++ src/main/java/core/ms/client/infra/domain/Client.java
@@ -1,8 +1,20 @@
 package core.ms.client.infra.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
 import java.io.Serializable;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
 public class Client implements Serializable {
 
     private Long id;
+    private String name;
 }
